f5f34fa0c028f3bcb700bb5ba112cc831c512c49
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-01 12:12:50 +0200

    refactor(details-mfe, auth-mfe): rename root selector to <app-details> and <app-auth>
    - Changed AppComponent selector from 'app-root' to 'app-details' and 'app-auth'
    - Updated index.html to bootstrap using <app-details> tag and <app-auth> tag
    - Preps for clearer DevTools inspection and avoids host selector conflict

--- projects/details-mfe/src/app/app.component.ts
+++ projects/details-mfe/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-details',
   standalone: true,
   imports: [RouterOutlet],
   template: `<router-outlet></router-outlet>`,

019d0f077c2797a43606751424bdeaa39a13834a
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-03 14:28:49 +0200

    feat(auth): implement DynamicFormComponent form markup, CommonModule import, and initialization logic

--- projects/auth-mfe/src/app/components/dynamic-form/dynamic-form.component.ts
+++ projects/auth-mfe/src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../shared/material.module';
+
 export interface FormFieldConfig {
   name: string;
   type: string;
   label: string;
-  placeholder: string;
-  validators: any[];
+  placeholder?: string;
+  validators?: any[];
 }
+
 @Component({
   selector: 'am-dynamic-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MaterialModule, CommonModule],
   templateUrl: './dynamic-form.component.html',
-  styleUrl: './dynamic-form.component.scss',
 })
-export class DynamicFormComponent {
-  config!: FormFieldConfig[];
+export class DynamicFormComponent implements OnInit {
+  @Input() config: FormFieldConfig[] = [];
+  @Input() submitLabel = 'Submit';
+  @Output() submitted = new EventEmitter<any>();
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    const group: Record<string, any> = {};
+    this.config.forEach((f) => {
+      group[f.name] = ['', f.validators || []];
+    });
+    this.form = this.fb.group(group);
+  }
+
+  onSubmit() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      this.submitted.emit(this.form.value);
+    }
+  }
 }

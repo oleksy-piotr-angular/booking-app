66e10b121eff0dfa73adf17cacab33e44753b7b9
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-04 00:00:29 +0200

    feat(shared): implement InlineErrorsComponent to render validation messages

--- projects/auth-mfe/src/app/shared/inline-errors/inline-errors.component.ts
+++ projects/auth-mfe/src/app/shared/inline-errors/inline-errors.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'am-inline-errors',
   standalone: true,
-  imports: [],
-  templateUrl: './inline-errors.component.html',
-  styleUrl: './inline-errors.component.scss'
+  imports: [MaterialModule],
+  template: `@if (control.errors && (control.dirty || control.touched)) { @for
+    (key of objectKeys(control.errors); track key) {
+    <mat-error>{{ errorMessages[key] || key }}</mat-error>
+    } }`,
 })
 export class InlineErrorsComponent {
-
+  @Input() control!: AbstractControl;
+  @Input() errorMessages: Record<string, string> = {};
+  objectKeys = Object.keys;
 }

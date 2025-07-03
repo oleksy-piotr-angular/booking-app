8a14f64e80cae3105d16dba71a97696520c601e3
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-03 11:41:42 +0200

    feat(auth): scaffold RegisterFormComponent

---
+++ projects/auth-mfe/src/app/components/register-form/register-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'am-register-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-form.component.html',
+  styleUrl: './register-form.component.scss'
+})
+export class RegisterFormComponent {
+
+}

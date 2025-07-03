061d2da369db84473582903cab6903a7509e460f
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-03 12:59:13 +0200

    feat(auth): implement InlineErrorsComponent to render control errors

--- projects/auth-mfe/src/app/shared/inline-errors/inline-errors.component.ts
+++ projects/auth-mfe/src/app/shared/inline-errors/inline-errors.component.ts
@@ -1,14 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material.module';
 @Component({
   selector: 'am-inline-errors',
   standalone: true,
-  imports: [],
-  templateUrl: './inline-errors.component.html',
-  styleUrl: './inline-errors.component.scss',
+  imports: [CommonModule, MaterialModule],
+  template: `
+    <ng-container *ngIf="control?.errors as errs">
+      <mat-error *ngFor="let key of objectKeys(errs)">
+        {{ errorMessages[key] || key }}
+      </mat-error>
+    </ng-container>
+  `,
 })
 export class InlineErrorsComponent {
   @Input() control!: AbstractControl;
   @Input() errorMessages: Record<string, string> = {};
+  objectKeys = Object.keys;
 }

401ed967a5ed66306458effc9d4fcd8c715b2bc4
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-05 00:16:14 +0200

    feat(auth): handle and display login errors in LoginComponent

--- projects/auth-mfe/src/app/pages/login/login.component.ts
+++ projects/auth-mfe/src/app/pages/login/login.component.ts
@@ -17,11 +17,19 @@ import { AuthService, LoginPayload } from '../../services/auth/auth.service';
       (submitted)="onSubmit($event)"
     >
     </am-dynamic-form>
+
+    @if(errorMsg){
+    <div class="error-message mat-error">
+      {{ errorMsg }}
+    </div>
+    }
   `,
 })
 export class LoginComponent {
   private readonly auth = inject(AuthService);
   private readonly router = inject(Router);
+
+  public errorMsg: string | null = null;
 
   public config: FormFieldConfig[] = [
     { name: 'email', type: 'email', label: 'Email' },
@@ -29,8 +37,12 @@ export class LoginComponent {
   ];
 
   public onSubmit(value: LoginPayload): void {
-    this.auth.login(value).subscribe(() => {
-      this.router.navigate(['/dashboard']);
+    this.errorMsg = null;
+    this.auth.login(value).subscribe({
+      next: () => this.router.navigate(['/dashboard']),
+      error: (err) => {
+        this.errorMsg = err.error?.message || 'Login failed';
+      },
     });
   }
 }

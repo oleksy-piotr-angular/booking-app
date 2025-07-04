fb8b69d6477579a225a9af4e3238abe599382764
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-04 20:19:27 +0200

    feat(auth): implement isTokenExpired() to check JWT exp field

--- projects/auth-mfe/src/app/shared/utils/jwt.util.ts
+++ projects/auth-mfe/src/app/shared/utils/jwt.util.ts
@@ -1,3 +1,12 @@
 export function isTokenExpired(token: string): boolean {
-  return false;
+  try {
+    const [, payload] = token.split('.');
+    const decoded = JSON.parse(atob(payload));
+    if (!decoded.exp) return true;
+
+    const now = Math.floor(Date.now() / 1000);
+    return decoded.exp < now;
+  } catch {
+    return true;
+  }
 }

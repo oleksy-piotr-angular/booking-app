2387dab05721cf5f1aaa64bd13a47971341d56ee
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-04 20:06:26 +0200

    refactor(auth): migrate authInterceptor spec to functional HttpInterceptorFn setup test(auth): use url.endsWith matcher for more robust interceptor test

--- projects/auth-mfe/src/app/interceptors/auth.interceptor.spec.ts
+++ projects/auth-mfe/src/app/interceptors/auth.interceptor.spec.ts
@@ -1,52 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 import {
-  HTTP_INTERCEPTORS,
   HttpClient,
-  HttpInterceptorFn,
+  provideHttpClient,
+  withInterceptors,
 } from '@angular/common/http';
-
 import {
   HttpClientTestingModule,
   HttpTestingController,
+  provideHttpClientTesting,
 } from '@angular/common/http/testing';
-import { TokenService } from '../services/token/token.service';
-import { authInterceptor } from './auth.interceptor';
 
-describe('authInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) =>
-    TestBed.runInInjectionContext(() => authInterceptor(req, next));
+import { authInterceptor } from './auth.interceptor';
+import { TokenService } from '../services/token/token.service';
+
+describe('authInterceptor (function)', () => {
   let http: HttpClient;
   let httpMock: HttpTestingController;
-  let tokenService: jasmine.SpyObj<TokenService>;
+  let tokenSpy: jasmine.SpyObj<TokenService>;
 
   beforeEach(() => {
-    tokenService = jasmine.createSpyObj('TokenService', ['getToken']);
-    tokenService.getToken.and.returnValue('test-token');
+    tokenSpy = jasmine.createSpyObj('TokenService', ['getToken']);
+    tokenSpy.getToken.and.returnValue('test-token');
 
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
       providers: [
-        { provide: TokenService, useValue: tokenService },
-        {
-          provide: HTTP_INTERCEPTORS,
-          useValue: authInterceptor,
-          multi: true,
-        },
+        { provide: TokenService, useValue: tokenSpy },
+        provideHttpClient(withInterceptors([authInterceptor])),
+        provideHttpClientTesting(),
       ],
+      imports: [HttpClientTestingModule],
     });
 
     http = TestBed.inject(HttpClient);
     httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
-  });
-
-  it('should add Authorization header with token', () => {
+  it('adds Authorization header with token', () => {
     http.get('/test').subscribe();
-    const req = httpMock.expectOne('/test');
-
+    const req = httpMock.expectOne((r) => r.url.endsWith('/test'));
+    httpMock.match((req) => {
+      console.log('Intercepted:', req.url);
+      return false; // don’t match anything
+    });
     expect(req.request.headers.get('Authorization')).toBe('Bearer test-token');
     req.flush({});
   });

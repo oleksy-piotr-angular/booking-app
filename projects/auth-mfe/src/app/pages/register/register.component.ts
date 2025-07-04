9240fbaaba414b3cc1f03760e312ef9eadd3d835
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-04 14:37:35 +0200

    chore(auth): scaffold standalone RegisterFormComponent under pages directory instead of components directory

---
+++ projects/auth-mfe/src/app/pages/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'am-register-form',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      register works!
+    </p>
+  `,
+  styles: ``
+})
+export class RegisterComponent {
+
+}

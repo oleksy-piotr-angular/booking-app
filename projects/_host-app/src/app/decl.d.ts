ea53c64da73f2c44eb24156208fb27f370cc241a
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-05 22:16:49 +0200

    chore(host): add TypeScript declaration for auth-mfe static federation

--- projects/_host-app/src/app/decl.d.ts
+++ projects/_host-app/src/app/decl.d.ts
@@ -1,3 +1,8 @@
 //As we are going with static federation, we also need typings for all
 // configured paths (EcmaScript modules) referencing Micro Frontends:
-declare module 'authMfe/*';
+declare module 'auth-mfe' {
+  export class AuthService {
+    isAuthenticated$: import('rxjs').Observable<boolean>;
+    // Add other methods/properties as needed
+  }
+}

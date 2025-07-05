efa6086009f1188e0c7f149a9625a06bb3d8508c
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-05 21:57:21 +0200

    Rename "app.routes.ts" to "host.routes.ts"

--- projects/_host-app/src/app/app.config.ts
+++ projects/_host-app/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { HOST_ROUTES } from './app.routes';
+import { HOST_ROUTES } from './host.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideAnimations(), provideRouter(HOST_ROUTES)],

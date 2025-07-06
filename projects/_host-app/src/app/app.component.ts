72a9afa2a4adc525dfc595e2d9005de3b35991c1
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-06 14:09:13 +0200

    refactor(host-app): bind <h1> to title property
    - change hard-coded '🚪 Host App' to {{ title }}
    - update default title value to 'Booking App - Host'

--- projects/_host-app/src/app/app.component.ts
+++ projects/_host-app/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'host-app';
+  title = 'Booking App - Host';
 }

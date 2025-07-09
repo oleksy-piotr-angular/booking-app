9e7f4b2793af89882095941a520c4c1abbb3ad18
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-09 22:45:08 +0200

    feat(navbar): add NgIf and AsyncPipe to standalone component imports (Specs now pass)

--- projects/_host-app/src/app/components/navbar/navbar.component.ts
+++ projects/_host-app/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject, Inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AUTH_MFE_SERVICE, IAuthService } from '../../tokens/auth.token';
 import { MaterialModule } from '../../shared/material.module';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, NgIf, AsyncPipe],
   template: `
     <mat-toolbar>
       <button
@@ -37,7 +38,7 @@ import { MaterialModule } from '../../shared/material.module';
 export class NavbarComponent {
   private router: Router = inject(Router);
   // Inject the AUTH_SERVICE token to get the auth service instance
-  private auth: IAuthService = inject(AUTH_MFE_SERVICE);
+  public auth: IAuthService = inject(AUTH_MFE_SERVICE);
 
   logout(): void {
     this.auth.logout();

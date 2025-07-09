32801b0fcca4d2d415e30518d3623c015db41075
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-09 16:53:52 +0200

    refactor(host-auth-guard): inject AUTH_SERVICE token instead of direct import from MFE

--- projects/_host-app/src/app/guards/host-auth.guard.ts
+++ projects/_host-app/src/app/guards/host-auth.guard.ts
@@ -9,12 +9,13 @@ import {
 } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { AUTH_SERVICE, IAuthService } from '../tokens/auth.token';
 
-// TYPE‐ONLY import of AuthService’s interface
+// TYPE‐ONLY import of AuthServices interface
 import type { AuthService } from 'auth-mfe';
 
-// create a token keyed to that interface
-export const AUTH_MFE_SERVICE = new InjectionToken<AuthService>('AuthService');
+// Assign created token keyed to that interface
+export const AUTH_MFE_SERVICE = AUTH_SERVICE as InjectionToken<IAuthService>;
 
 const LOGIN_PATH = ['/auth', 'login'];
 
@@ -22,8 +23,8 @@ export const hostAuthGuard: CanMatchFn = (
   _route: Route,
   _segments: UrlSegment[]
 ): Observable<boolean | UrlTree> => {
-  // inject the token — at runtime you’ll bind it to the real class,
-  // in tests we’ll bind it to a stub
+  // inject the token — at runtime you will bind it to the real class,
+  // in tests we will bind it to a stub
   const auth = inject(AUTH_MFE_SERVICE);
   const router = inject(Router);
 

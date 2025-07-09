7fefa021db29e779106377c8809d75ad5b2e934d
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-09 22:29:16 +0200

    feat(navbar): implement Material toolbar with auth-driven login/logout buttons

--- projects/_host-app/src/app/components/navbar/navbar.component.ts
+++ projects/_host-app/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AUTH_MFE_SERVICE, IAuthService } from '../../tokens/auth.token';
+import { MaterialModule } from '../../shared/material.module';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
-  template: ``,
+  imports: [MaterialModule],
+  template: `
+    <mat-toolbar>
+      <button
+        mat-button
+        *ngIf="!(auth.isAuthenticated$ | async)"
+        routerLink="/auth/login"
+      >
+        Login
+      </button>
+      <button
+        mat-button
+        *ngIf="!(auth.isAuthenticated$ | async)"
+        routerLink="/auth/register"
+      >
+        Register
+      </button>
+      <button
+        mat-button
+        class="logout"
+        *ngIf="auth.isAuthenticated$ | async"
+        (click)="logout()"
+      >
+        Logout
+      </button>
+    </mat-toolbar>
+  `,
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  private router: Router = inject(Router);
+  // Inject the AUTH_SERVICE token to get the auth service instance
+  private auth: IAuthService = inject(AUTH_MFE_SERVICE);
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['/auth/login']);
+  }
+}

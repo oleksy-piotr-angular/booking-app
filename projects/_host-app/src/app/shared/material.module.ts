2fe3c7e470869ef8a60ac94e6aa9e13ec7833334
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-09 22:23:19 +0200

    feat(material): extend MaterialModule with MatToolbarModule and export UI components

--- projects/_host-app/src/app/shared/material.module.ts
+++ projects/_host-app/src/app/shared/material.module.ts
@@ -1,10 +1,11 @@
 // material.module.ts
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Type } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MATERIAL = [
   MatFormFieldModule,
@@ -12,7 +13,11 @@ const MATERIAL = [
   MatButtonModule,
   MatSnackBarModule,
   MatTooltipModule,
+  MatToolbarModule,
 ];
 
-@NgModule({ exports: MATERIAL })
+@NgModule({
+  imports: MATERIAL,
+  exports: MATERIAL,
+})
 export class MaterialModule {}

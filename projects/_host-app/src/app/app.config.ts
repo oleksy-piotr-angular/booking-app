0374fd8701356062798f20c08ac7902104f1a570
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-11 01:27:08 +0200

    refactor(http): use provideHttpClient(withInterceptors) instead of HttpClientModule
    - Host & MFE now call provideHttpClient(withInterceptors([authInterceptor]))
    - Remove importProvidersFrom(HttpClientModule)

--- projects/_host-app/src/app/app.config.ts
+++ projects/_host-app/src/app/app.config.ts
@@ -1,5 +1,5 @@
 // app.config.ts
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
@@ -7,14 +7,18 @@ import {
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { HOST_ROUTES } from './host.routes';
-import { HttpClientModule } from '@angular/common/http';
-import { AUTH_MFE_SERVICE } from './tokens/auth.token';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AUTH_MFE_SERVICE } from '@booking-app/auth-token';
 import { AuthService } from '../../../auth-mfe/src/app/services/auth/auth.service';
+import { authInterceptor } from 'projects/auth-mfe/src/app/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    importProvidersFrom(HttpClientModule), // HTTP so AuthService can work
+    provideHttpClient(
+      //
+      withInterceptors([authInterceptor])
+    ), // HTTP so AuthService can work
     provideRouter(HOST_ROUTES, withEnabledBlockingInitialNavigation()),
     { provide: AUTH_MFE_SERVICE, useExisting: AuthService }, // Bind the token to the real remote class
   ],
--- projects/auth-mfe/src/app/app.config.ts
+++ projects/auth-mfe/src/app/app.config.ts
@@ -1,10 +1,23 @@
+import { RegisterComponent } from './pages/register/register.component';
 // app.config.ts
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { AUTH_ROUTES } from './auth.routes';
+import { AUTH_MFE_SERVICE } from '@booking-app/auth-token';
+import { AuthService } from './services/auth/auth.service';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideRouter(AUTH_ROUTES)],
+  providers: [
+    provideAnimations(),
+    provideHttpClient(
+      //
+      withInterceptors([authInterceptor])
+    ), // HTTP so AuthService can work
+    provideRouter(AUTH_ROUTES),
+    { provide: AUTH_MFE_SERVICE, useExisting: AuthService },
+  ],
 };

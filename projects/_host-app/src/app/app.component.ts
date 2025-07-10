cb38dd2834fc1f4a7765f38db2d7c091204f16e2
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-10 13:48:39 +0200

    feat(app): update title to 'Booking App - DEMO' and render in template

--- projects/_host-app/src/app/app.component.ts
+++ projects/_host-app/src/app/app.component.ts
@@ -11,5 +11,5 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Booking App - Host';
+  title = 'Booking App - DEMO';
 }

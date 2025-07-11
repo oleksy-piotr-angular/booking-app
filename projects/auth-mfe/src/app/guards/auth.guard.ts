45b6fadc60bf9166d6b4c931b29558448e26d900
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-12 01:26:03 +0200

    refactor(auth-guard): switch to reactive isAuthenticated$ guard

--- projects/auth-mfe/src/app/guards/auth.guard.ts
+++ projects/auth-mfe/src/app/guards/auth.guard.ts
@@ -1,17 +1,16 @@
 // auth.guard.ts
-import { inject } from '@angular/core';
 import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { inject } from '@angular/core';
+import { IAuthService, AUTH_MFE_SERVICE } from '@booking-app/auth-token';
 import { map } from 'rxjs/operators';
-import { AUTH_MFE_SERVICE, IAuthService } from '@booking-app/auth-token';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
   const auth = inject(AUTH_MFE_SERVICE) as IAuthService;
   const router = inject(Router);
 
   return auth.isAuthenticated$.pipe(
-    map((isAuth) => {
-      console.log('authGuard isAuthenticated$', isAuth);
-      return isAuth ? true : (router.createUrlTree(['/login']) as UrlTree);
-    })
+    map((isAuth) =>
+      isAuth ? true : (router.createUrlTree(['/auth', 'login']) as UrlTree)
+    )
   );
 };

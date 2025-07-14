628d50e7bbb4972f06f61c529fe307a8240c5687
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-15 01:26:09 +0200

    refactor(auth-token): export DTOs, models, mappers and token interface in public-api

--- libs/auth-token/src/public-api.ts
+++ libs/auth-token/src/public-api.ts
@@ -5,3 +5,7 @@
 export * from './lib/auth.token';
 export * from './lib/token.token';
 export * from './lib/auth.interface';
+export * from './lib/token.interface';
+export * from './lib/dtos/auth.dto';
+export * from './lib/models/auth.model';
+export * from './lib/mappers/auth.mapper';

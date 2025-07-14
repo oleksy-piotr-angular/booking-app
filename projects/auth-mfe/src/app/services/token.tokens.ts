a14ac09ee1cf05a851515dd0b52fd6f55a8ba787
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-14 16:44:31 +0200

    chore(auth-mfe): remove redundant auth.tokens.ts and token.tokens.ts

--- projects/auth-mfe/src/app/services/token.tokens.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export interface ITokenService {
-  getToken(): string | null;
-  setToken(token: string): void;
-  clearToken(): void;
-}
-
-export const TOKEN_SERVICE = new InjectionToken<ITokenService>('TOKEN_SERVICE');

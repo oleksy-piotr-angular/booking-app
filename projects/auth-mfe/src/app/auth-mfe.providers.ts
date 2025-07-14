f8a99c96c7566bc0c4f4e65ac05f5a87640ad8c1
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-07-14 16:44:18 +0200

    feat(auth-mfe): add AUTH_MFE_PROVIDERS array for token wiring

--- projects/auth-mfe/src/app/auth-mfe.providers.ts
+++ projects/auth-mfe/src/app/auth-mfe.providers.ts
@@ -1,9 +1,13 @@
-// auth-mfe.providers.ts
-import { AUTH_MFE_SERVICE } from '@booking-app/auth-token';
+// projects/auth-mfe/src/app/remote-entry/auth-mfe.providers.ts
+import { Provider } from '@angular/core';
 import { AuthService } from './services/auth/auth.service';
-import type { Provider } from '@angular/core';
+import { TokenService } from './services/token/token.service';
+import { AUTH_MFE_SERVICE } from '@booking-app/auth-token';
+import { TOKEN_MFE_SERVICE } from '@booking-app/auth-token';
 
 export const AUTH_MFE_PROVIDERS: Provider[] = [
-  // when DI sees AUTH_MFE_SERVICE, give the same AuthService instance
+  AuthService,
   { provide: AUTH_MFE_SERVICE, useExisting: AuthService },
+  TokenService,
+  { provide: TOKEN_MFE_SERVICE, useExisting: TokenService },
 ];
